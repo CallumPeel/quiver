ed0deacaad6f77b83173348373efdf86b8ec8bc8
Callum <callum.epeel@gmail.com>
2022-06-05 05:48:45 +0800

    Math and Menu
    Cleaned up

--- Quiver/ImageProcessor.h
+++ Quiver/ImageProcessor.h
@@ -4,7 +4,6 @@
 
 #include <stdint.h>
 #include "WorldView.h"
-#include "Camera.h"
 
 typedef struct {
     int width;
---
+++ Quiver/MathExt.c
@@ -0,0 +1,11 @@
+#include "MathExt.h"
+
+double DTR(double deg)
+{
+    return deg * M_PI / 180.0;
+}
+
+double RTD(double rad)
+{
+    return rad * 180.0 / M_PI;
+}
